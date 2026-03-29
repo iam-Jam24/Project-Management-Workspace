587e88054ab042a2ec510ecfa70559c0c6b5ce00
JAMAL RAZA <asjamalraza@gmail.com>
2026-03-29 14:32:15 +0530

    added initial routes

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import { Toaster } from "react-hot-toast";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Team from "./pages/Team";
+import ProjectDetails from "./pages/ProjectDetails";
+import TaskDetails from "./pages/TaskDetails";
+import LandingPage from "./pages/LandingPage";
+
+const App = () => {
+    return (
+        <>
+            <Toaster />
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route element={<Layout />}>
+                    <Route path="dashboard" element={<Dashboard />} />
+                    <Route path="team" element={<Team />} />
+                    <Route path="projects" element={<Projects />} />
+                    <Route path="projectsDetail" element={<ProjectDetails />} />
+                    <Route path="taskDetails" element={<TaskDetails />} />
+                </Route>
+            </Routes>
+        </>
+    );
+};
+
+export default App;
